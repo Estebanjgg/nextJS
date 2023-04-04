37f9a734426ef7e998d4f8f6a23e85b0c8836d14
Esteban Jose Gonzalez Gomez <91167097+Estebanjgg@users.noreply.github.com>
2023-04-04 00:07:22 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
+import Head from 'next/head'
 import GlobalStyle from "../src/theme/GlobalStyle";
-
-
 
 
 export default function MyApp({ Component, pageProps }) {
     return ( 
         <>
+        <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;1,400&family=Roboto+Slab:wght@100;300&family=Sono:wght@200;500&display=swap" rel="stylesheet"/>
+        </Head>
         <GlobalStyle/>
         <Component {...pageProps} />
 
@@ -16,4 +20,6 @@ export default function MyApp({ Component, pageProps }) {
     
   
   }
+
+
 
