82fa0111a8557f60f77e61edec4baa929748f31d
Esteban Jose Gonzalez Gomez <91167097+Estebanjgg@users.noreply.github.com>
2023-04-04 01:36:14 -0300

    feat: add new feature

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,36 @@
-import Head from 'next/head'
-import GlobalStyle from "../src/theme/GlobalStyle";
+import Head from 'next/head';
+import Script from 'next/script';
 
+import GlobalStyle from '../src/theme/GlobalStyle';
 
-export default function MyApp({ Component, pageProps }) {
-    return ( 
+function MyApp({ Component, pageProps }) {
+    return (
         <>
-        <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;1,400&family=Roboto+Slab:wght@100;300&family=Sono:wght@200;500&display=swap" rel="stylesheet"/>
-        </Head>
-        <GlobalStyle/>
-        <Component {...pageProps} />
-
-                </>
-        
-        
-        )
-    
-  
-  }
-
+            <Head>
+                {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-2QZDPX3BLK"></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                      
+                        gtag('config', 'G-0H08QBX8EL');
+                        `
+                    }}
+                />
 
 
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true.toString()} />
+                <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet" />
+            </Head>
+            <Script src="https://polyfill.io/v3/polyfill.min.js" strategy="beforeInteractive" />
+            <GlobalStyle />
+            <Component {...pageProps} />
+        </>
+    )
+}
+
+export default MyApp;
